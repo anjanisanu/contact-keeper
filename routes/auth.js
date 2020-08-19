c0a0e92f315f080d0325c09fad6ccd8a9b6d2a91
Anjani Sanu <anjanikrsanu@gmail.com>
2020-08-19 20:19:19 +0530

    Added Login functionality and middleware for accessing private routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+const auth = require('./../middleware/auth');
+const User = require('./../models/User');
 
 const router = express.Router();
 
@@ -7,8 +13,14 @@ const router = express.Router();
 @desc   Get login user details
 @access Private
 */
-router.get('/', (req, res) => {
-	res.send('Get login details');
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('name email -_id');
+		res.status(200).json(user);
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 /*
@@ -16,8 +28,28 @@ router.get('/', (req, res) => {
 @desc   Login User
 @access Public
 */
-router.get('/', (req, res) => {
-	res.send('Login User');
+router.post('/', async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		if (!email || !password || email === '' || password === '')
+			return res.status(400).json({ msg: 'Please enter email id and password' });
+		const user = await User.findOne({ email });
+		if (!user) return res.status(400).json({ msg: 'Invalid Credentials' });
+
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) return res.status(400).json({ msg: 'Invalid Credentials' });
+
+		const payload = { user: { id: user.id } };
+		jwt.sign(payload, config.get('jwtSecret'), { expiresIn: '90d' }, (err, token) => {
+			if (err) throw err;
+
+			res.status(201).json({ token });
+		});
+	} catch (err) {
+		console.log(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
