f4f9d08af745d7573a1a6d34a84371a5ec864dee
Anjani Sanu <anjanikrsanu@gmail.com>
2020-08-25 20:17:16 +0530

    Some deployment fixes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 
 const connectDB = require('./config/db');
 
@@ -12,9 +13,15 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
+app.use(express.static(path.join(__dirname, '../build')));
+
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, './client/build'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
