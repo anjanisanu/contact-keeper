0e0be90b80d371713756ee73144e14947dfe676f
Anjani Sanu <anjanikrsanu@gmail.com>
2020-08-27 13:40:53 +0530

    Added Spinner while loading contacts

--- client/src/components/contacts/ContactItems.js
+++ client/src/components/contacts/ContactItems.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import { connect } from 'react-redux';
 
 import { getContacts } from './../../actions/contactActions';
 import ContactItem from './ContactItem';
+import Spinner from './../layout/Spinner';
 
 const ContactItems = ({ contacts, getContacts }) => {
 	useEffect(() => {
@@ -10,20 +11,26 @@ const ContactItems = ({ contacts, getContacts }) => {
 		//eslint-disable-next-line
 	}, []);
 	return (
-		<section className='cards'>
-			<div className='filter'>
-				<form className='filter__form'>
-					<div className='filter__form--field'>
-						<svg className='filter__form--field--icon'>
-							<use xlinkHref='img/sprite.svg#icon-magnifying-glass' />
-						</svg>
-						<input type='text' placeholder='Filter Contacts' className='filter__form--field--input' />
+		<Fragment>
+			{contacts !== null ? (
+				<section className='cards'>
+					<div className='filter'>
+						<form className='filter__form'>
+							<div className='filter__form--field'>
+								<svg className='filter__form--field--icon'>
+									<use xlinkHref='img/sprite.svg#icon-magnifying-glass' />
+								</svg>
+								<input type='text' placeholder='Filter Contacts' className='filter__form--field--input' />
+							</div>
+						</form>
 					</div>
-				</form>
-			</div>
 
-			{contacts.map((contact) => <ContactItem key={contact._id} contact={contact} />)}
-		</section>
+					{contacts.map((contact) => <ContactItem key={contact._id} contact={contact} />)}
+				</section>
+			) : (
+				<Spinner />
+			)}
+		</Fragment>
 	);
 };
 
